145c403923f3ccf99322165f71d172b7e2778890
Rebekah <rebekahmenn@gmail.com>
2023-08-25 12:39:17 -0400

    undirected, unweighted graph

---
+++ dataStructures/graph.js
@@ -0,0 +1,50 @@
+// UNDIRECTED, UNWEIGHTED GRAPH
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  // if vertex does not exist in my adjacencyList
+  // add the key/value pair with the vertex as key and empty array as value
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+  // REMOVE AN EDGE
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
+  }
+  // DELETE KEY FROM AN OBJECT
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+}
+
+let g = new Graph();
+g.addVertex("Dallas");
+g.addVertex("Tokyo");
+g.addVertex("Aspen");
+g.addVertex("Los Angeles");
+g.addVertex("Hong Kong");
+g.addEdge("Dallas", "Tokyo");
+g.addEdge("Dallas", "Aspen");
+g.addEdge("Hong Kong", "Tokyo");
+g.addEdge("Hong Kong", "Dallas");
+g.addEdge("Los Angeles", "Hong Kong");
+g.addEdge("Los Angeles", "Aspen");
+console.log(g);
+g.removeEdge("Dallas", "Aspen");
+g.removeVertex("Aspen");
+console.log(g);
