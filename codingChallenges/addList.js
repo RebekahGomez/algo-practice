ba0e4af10d588ebd42f78e1857175cce8d88d83b
Rebekah <rebekahmenn@gmail.com>
2023-09-04 17:20:23 -0400

    second option with addList

--- codingChallenges/addList.js
+++ codingChallenges/addList.js
@@ -8,6 +8,7 @@
 // add(1, 50, 1.23) //=> 52.23
 // add(7, -12) //=> -5
 
+// OPTION 1
 function addList(...nums) {
   let sum = 0;
   for (let i = 0; i < nums.length; i++) {
@@ -21,3 +22,18 @@ console.log(addList(1, 2, 3));
 // NOTE: when you use the rest parameter (...nums), JavaScript automatically gathers the arguments
 // passed to the function into an array named "nums" even though we didn't explicitely pass an array when
 // we called the function.
+
+// OPTION 2 - PRE-ES6 SYNTAX W/ IMPLICIT ARGUMENTS
+function addList() {
+  if (arguments.length === 0) {
+    return 0;
+  } else {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+    }
+    return sum;
+  }
+}
+
+console.log(addList(1, 2, 3, 4));
