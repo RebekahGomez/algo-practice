942e3eb8d7a0c22ff6e2e9e6ae3a2e383c9138fd
Rebekah <rebekahmenn@gmail.com>
2023-09-10 16:07:35 -0400

    touch 07_reverseUpcaseString

---
+++ codingChallenges/07_reverseUpcaseString.js
@@ -0,0 +1,5 @@
+// Prompt:
+// - Write a function called reverseUpcaseString that accepts a single string argument,
+//   then returns the string with its characters in reverse order and converts all characters to uppercase.
+// Examples:
+// reverseUpcaseString("SEI Rocks!"); //=> "!SKCOR IES"
