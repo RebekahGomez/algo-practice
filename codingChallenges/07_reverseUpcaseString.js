9152810ea8ebeec6db058b7299144e332d48f731
Rebekah <rebekahmenn@gmail.com>
2023-09-10 16:10:32 -0400

    set string toUpperCase()

--- codingChallenges/07_reverseUpcaseString.js
+++ codingChallenges/07_reverseUpcaseString.js
@@ -3,3 +3,10 @@
 //   then returns the string with its characters in reverse order and converts all characters to uppercase.
 // Examples:
 // reverseUpcaseString("SEI Rocks!"); //=> "!SKCOR IES"
+
+function reverseUpcaseString(singleString) {
+  let result = singleString.toUpperCase();
+  return result;
+}
+
+console.log(reverseUpcaseString("hello"));
