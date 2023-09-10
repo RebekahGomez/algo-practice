fb990438fcfe75009cf284cfcc2aac8a6130233f
Rebekah <rebekahmenn@gmail.com>
2023-09-10 17:17:20 -0400

    using slice method

--- codingChallenges/08_removeEnds.js
+++ codingChallenges/08_removeEnds.js
@@ -7,15 +7,29 @@
 // removeEnds('a'); //=> "" (empty string)
 
 // OPTION 1 USING A FOR LOOP
+// function removeEnds(str) {
+//   if (str.length < 3) {
+//     return "";
+//   }
+//   let result = "";
+//   for (let i = 1; i < str.length - 1; i++) {
+//     result += str[i];
+//   }
+//   return result;
+// }
+
+// console.log(removeEnds("hello"));
+
+// OPTION 1 USING .SLICE()
 function removeEnds(str) {
   if (str.length < 3) {
     return "";
   }
-  let result = "";
-  for (let i = 1; i < str.length - 1; i++) {
-    result += str[i];
-  }
+  let result = str.slice(1, -1);
   return result;
 }
 
-console.log(removeEnds("hello"));
+console.log(removeEnds("goodbye"));
+// .slice() method is an array instance that returns a shallow copy of a portion of an array into a new array object
+// from start to end (not including the end) where start and end represent the index of items in that array.
+// The original array will not be modified.
