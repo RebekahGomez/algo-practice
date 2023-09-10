4aaed7965420c9e11565cc42641ed0d701aaa87a
Rebekah <rebekahmenn@gmail.com>
2023-09-10 14:12:20 -0400

    initial answer for computeRemainder

---
+++ codingChallenges/05_computeRemainder.js
@@ -0,0 +1,17 @@
+// Prompt:
+// - Write a function named computeRemainder that accepts two numeric arguments
+//  and returns the remainder of the division of those two numbers.
+// - The first argument should be the dividend and the second argument should be the divisor.
+// - If a 0 is passed in as the second argument you should return JavaScript's special numeric value: Infinity.
+// - For extra fun, complete this challenge without using the modulus (%) operator.
+// Examples:
+// computeRemainder(10,2) //=> 0
+// computeRemainder(4,0) //=> Infinity
+// computeRemainder(10.5, 3) //=> 1.5
+
+function computeRemainder(num1, num2) {
+  let answer = num1 % num2;
+  return answer;
+}
+
+console.log(computeRemainder(9, 3));
