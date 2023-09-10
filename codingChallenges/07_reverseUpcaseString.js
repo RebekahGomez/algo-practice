ac56bb15919b6a39cd1292e5712b643b81f4dedd
Rebekah <rebekahmenn@gmail.com>
2023-09-10 16:38:29 -0400

    uncommented all methods

--- codingChallenges/07_reverseUpcaseString.js
+++ codingChallenges/07_reverseUpcaseString.js
@@ -5,45 +5,45 @@
 // reverseUpcaseString("SEI Rocks!"); //=> "!SKCOR IES"
 
 // OPTION 1 BUILT-IN METHODS
-// function reverseUpcaseString(str) {
-//   let splitStr = str.split("");
-//   let reverseStr = splitStr.reverse("");
-//   let joinStr = reverseStr.join("");
-//   let result = joinStr.toUpperCase("");
-//   return result;
-// }
+function reverseUpcaseString(str) {
+  let splitStr = str.split("");
+  let reverseStr = splitStr.reverse("");
+  let joinStr = reverseStr.join("");
+  let result = joinStr.toUpperCase("");
+  return result;
+}
 
-// console.log(reverseUpcaseString("hello"));
+console.log(reverseUpcaseString("hello"));
 
 // OPTION 2 CHANING BUILT-IN METHODS
-// function reverseString(str) {
-//   return str.split("").reverse("").join("").toUpperCase("");
-// }
+function reverseString(str) {
+  return str.split("").reverse("").join("").toUpperCase("");
+}
 
-// console.log(reverseString("goodbye"));
+console.log(reverseString("goodbye"));
 
 // OPTION 3 W/ DECREMENTING FOR LOOP
-// function reverseString(str) {
-//   let newString = "";
-//   for (let i = str.length - 1; i >= 0; i--) {
-//     newString += str[i];
-//   }
-//   let upperString = newString.toUpperCase("");
-//   return upperString;
-// }
+function reverseString(str) {
+  let newString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  let upperString = newString.toUpperCase("");
+  return upperString;
+}
 
-// console.log(reverseString("sayWhat"));
+console.log(reverseString("sayWhat"));
 
 // OPTION 4 W/ RECURSION
-// function reverseString(str) {
-//   if (str === "") {
-//     return "";
-//   } else {
-//     return reverseString(str.substr(1)) + str.charAt(0).toUpperCase();
-//   }
-// }
+function reverseString(str) {
+  if (str === "") {
+    return "";
+  } else {
+    return reverseString(str.substr(1)) + str.charAt(0).toUpperCase();
+  }
+}
 
-// console.log(reverseString("hello"));
+console.log(reverseString("gotcha"));
 
 // OPTION 5 TERNARY OPERATOR
 function reverseString(str) {
