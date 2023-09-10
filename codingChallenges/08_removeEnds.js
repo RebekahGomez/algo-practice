50f3c79ac0cb83dcc32a082e8a6cbb1cb982b6d7
Rebekah <rebekahmenn@gmail.com>
2023-09-10 16:40:04 -0400

    touch 08_removeEnds

---
+++ codingChallenges/08_removeEnds.js
@@ -0,0 +1,7 @@
+// Prompt:
+// - Write a function called removeEnds that accepts a single string argument,
+//   then returns the a string with the first and last characters removed.
+// - If the length of the string argument is less than 3, return an empty string.
+// Examples:
+// removeEnds('SEI Rocks!'); //=> "EI Rocks"
+// removeEnds('a'); //=> "" (empty string)
