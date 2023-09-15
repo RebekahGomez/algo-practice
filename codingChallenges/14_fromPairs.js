937922c0f3c06454499a4ffb4b92e680f4514683
Rebekah <rebekahmenn@gmail.com>
2023-09-15 11:24:30 -0400

    added comments

--- codingChallenges/14_fromPairs.js
+++ codingChallenges/14_fromPairs.js
@@ -26,3 +26,10 @@ console.log(
     ["name", "Reebs"],
   ])
 );
+
+// Remember that to assign value to a key in an object, we use the following syntax:
+// objectName[keyVariable] = valueVariable;
+// so with the line: result[key] = value; we are assigning the key/value pairs in the result object
+// We cannot use dot notation in this function because in dot notation (EG: result.key = value),
+// JavaScript is looking for a property named "key" on the "result" object, rather than assigning
+// the key/value pair variables to the object
