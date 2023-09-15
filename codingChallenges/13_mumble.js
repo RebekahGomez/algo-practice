412f5ec25ca23713777dde296d7bad355564d402
Rebekah <rebekahmenn@gmail.com>
2023-09-15 10:04:41 -0400

    2nd function option

--- codingChallenges/13_mumble.js
+++ codingChallenges/13_mumble.js
@@ -11,15 +11,34 @@
 // mumble('!A 2'); //=> '!-AA-   -2222'
 
 // OPTION 1 W/ FOR LOOP
+// function mumble(str) {
+//   let result = "";
+//   for (let i = 0; i < str.length; i++) {
+//     result += str[i].repeat(i + 1);
+//     if (i !== str.length - 1) {
+//       result += "-";
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(mumble("abc"));
+
+// .repeat() method is used to construct and return a new string that contains a specified number of
+// copies of the original string. It concatenates th ese copies together. This method does not modify
+// the original string but returns a new string.
+// str.repeat(count); --> count is an integer between 0 and Infinity. It indicates the number of times
+// to repeat the original string.
+// EG: console.log('A'.repeat(3)) --> Outputs: 'AAA'
+
+// OPTION 2 W/ SPLIT, REPEAT, & JOIN
 function mumble(str) {
-  let result = "";
-  for (let i = 0; i < str.length; i++) {
-    result += str[i].repeat(i + 1);
-    if (i !== str.length - 1) {
-      result += "-";
-    }
+  let newStr = str.split("");
+  let arr = [];
+  for (let i = 0; i < newStr.length; i++) {
+    arr.push(newStr[i].repeat(i + 1));
   }
-  return result;
+  return arr.join("-");
 }
 
-console.log(mumble("abc"));
+console.log(mumble("cba"));
