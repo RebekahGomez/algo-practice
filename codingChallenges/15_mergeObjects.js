8da303e8122d6f67f40dcf7a83175bdeee23d8c5
Rebekah <rebekahmenn@gmail.com>
2023-09-17 21:23:03 -0400

    added comments

--- codingChallenges/15_mergeObjects.js
+++ codingChallenges/15_mergeObjects.js
@@ -10,18 +10,18 @@
 // mergeObjects({a: 1, b: 2, c: 3}, {d: 4}, {b: 22, d: 44});  //=> {a: 1, b: 22, c: 3, d: 44}
 
 // OPTION 1 W/ A FOR LOOP & SPREAD OPERATOR
-// function mergeObjects(...objects) {
-//   if (objects.length === 0) {
-//     return {};
-//   }
-//   let result = objects[0];
-//   for (let i = 1; i < objects.length; i++) {
-//     result = { ...result, ...objects[i] };
-//   }
-//   return result;
-// }
+function mergeObjects(...objects) {
+  if (objects.length === 0) {
+    return {};
+  }
+  let result = objects[0];
+  for (let i = 1; i < objects.length; i++) {
+    result = { ...result, ...objects[i] };
+  }
+  return result;
+}
 
-// console.log(mergeObjects({ a: 0 }, { a: 1, b: 22 }));
+console.log(mergeObjects({ a: 0 }, { a: 1, b: 22 }));
 
 // OPTION 2 W/ OBJECT.ASSIGN()
 function mergeObjects(...objects) {
@@ -32,3 +32,11 @@ function mergeObjects(...objects) {
 }
 
 console.log(mergeObjects({ a: 13, b: 22 }, { c: 55, d: 12, a: 0 }));
+
+// Object.assign() is another way to merge properties from multiple source objects into a target object.
+// It takes a target object and one or more source objects and then retgurns the target object.
+// The properties in the target object are overwritten by properties in the sources if they ahve the same key.
+// We still gather all provided objects into an array using the rest parameter '...objects'
+// Object.assign(...objects) spreads out the 'objects' array as individual arguments to Object.assign()
+// The properties of all the objects will be merges into the first object in the list, and this modified
+// first object is what Object.assign() returns
