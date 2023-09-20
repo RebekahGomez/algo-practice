8bcd2b1bb292d47482bf78c24a50ccab7e0b5bc8
Rebekah <rebekahmenn@gmail.com>
2023-09-20 12:24:23 -0400

    2nd option for findHighestPriced

--- codingChallenges/16_findHighestPriced.js
+++ codingChallenges/16_findHighestPriced.js
@@ -21,11 +21,37 @@
 // ]);
 // //=> { sku: 'b2', price: 50 }
 
+// OPTION 1 USING A FOR LOOP
+// function findHighestPriced(arr) {
+//   let highestPricedObject = arr[0];
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i].price > highestPricedObject.price) {
+//       highestPricedObject = arr[i];
+//     }
+//   }
+//   return highestPricedObject;
+// }
+
+// console.log(
+//   findHighestPriced([
+//     { sku: "a1", price: 25 },
+//     { sku: "b2", price: 5 },
+//     { sku: "c3", price: 50 },
+//     { sku: "d4", price: 10 },
+//   ])
+// );
+
+// OPTION 2 USING NESTED LOOP IF YOU DON'T KNOW THE KEY AHEAD OF TIME
 function findHighestPriced(arr) {
   let highestPricedObject = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i].price > highestPricedObject.price) {
-      highestPricedObject = arr[i];
+  let highestValue = Number.NEGATIVE_INFINITY;
+
+  for (let obj of arr) {
+    for (let key in obj) {
+      if (typeof obj[key] === "number" && obj[key] > highestValue) {
+        highestValue = obj[key];
+        highestPricedObject = obj;
+      }
     }
   }
   return highestPricedObject;
