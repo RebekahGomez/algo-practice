c4036f6355427230d219a04da3d9ea24c0951c6a
Rebekah <rebekahmenn@gmail.com>
2023-10-23 11:03:21 -0400

    added loops.js

---
+++ javascript/loops.js
@@ -0,0 +1,116 @@
+/*
+ * PART 1: `while` loops
+ */
+
+/*
+ * Prompt:
+ *
+ * Use a while loop to count from 0 to 10. Log each number to the console.
+ */
+
+/*
+ * Prompt:
+ *
+ * Use a while loop to count from 10 to 0. Log each number to the console.
+ */
+
+/*
+ * Prompt:
+ *
+ * Use `prompt()` to write a while loop that will continue to ask the
+ * user for the magic number until they enter the value "7". (What's prompt?
+ * Check MDN!)
+ */
+
+/*
+ * PART 2: `for` loops
+ */
+
+/*
+ * Prompt:
+ *
+ * Write a for loop that counts from 0 to 5. Log each number to the console.
+ */
+
+/*
+ * Prompt:
+ *
+ * Write a for loop that counts down from 5 to 0. Log each number to the
+ * console.
+ */
+
+/*
+ * Prompt:
+ *
+ * Write a for loop that counts from 0 to 10 in increments of 2. Log each number
+ * to the console.
+ */
+
+/*
+ * Prompt:
+ *
+ * Write a for loop that counts from -5 to 5, printing each number to the
+ * console.
+ */
+
+/*
+ * Prompt:
+ *
+ * Create a variable called sum (make sure sum is defined outside of your loop).
+ * Create a loop that counts from 1 to 10, incrementing by 1. This time, rather
+ * than printing i to the console, add i to sum. After your loop, print the
+ * value of sum to the console.
+ *
+ * Reflect: What did we just do here?
+ */
+
+/*
+ * Prompt:
+ *
+ * We can write any valid JavaScript inside the block of our loop (the part
+ * between the curly braces). Let's put a conditional in there!
+ *
+ * Create a loop that counts from 0 to 10. Inside your loop, write a conditional
+ * that matches these requirements:
+ *
+ *    - if the value of i is even, print out "i is even"
+ *    - otherwise, print out "i is odd"
+ */
+
+/*
+ * PART 3: Iterating through Arrays
+ */
+
+/*
+ * Prompt:
+ *
+ * Write a for loop that prints out each item in the below array.
+ */
+
+const letters = ["a", "b", "c", "d", "e", "f", "g"];
+
+/*
+ * Prompt:
+ *
+ * Write a for loop that sums the values in the below array. Print the sum out
+ * to the console.
+ */
+
+const numbers = [23, 19, 23, 52, 12];
+
+/*
+ * Prompt:
+ *
+ * Write a for loop that doubles each value in the following array. Print the
+ * array out to the console.
+ */
+
+const moreNumbers = [12, 23, 34, 45];
+
+/*
+ * Prompt:
+ *
+ * Write a for loop that sets a variable to the largest number in the following array. Print that number out to the console.
+ */
+
+const evenMoreNumbers = [12, 45, 23, 999, 7];
